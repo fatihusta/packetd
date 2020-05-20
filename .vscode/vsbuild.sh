dc0ca6ab24f4569a9f5baabca11882171a293162
John Sommerville <jsommerville@untangle.com>
2020-05-20 16:27:57 -0600

    dev-build: creating specialized dev dockerfile, run lint during make, pass race condition also, WIP on trying to get delve working in the docker container

--- .vscode/vsbuild.sh
+++ .vscode/vsbuild.sh
@@ -4,9 +4,13 @@
 ##
 TARGET=$1
 
-docker-compose -f build/docker-compose.build.yml up --build musl
-ssh root@$TARGET "/etc/init.d/packetd stop"; 
-sleep 5
-scp ./cmd/packetd/packetd root@$TARGET:/usr/bin/; 
-ssh root@$TARGET "/etc/init.d/packetd start"
+docker-compose -f build/docker-compose.build.yml up --exit-code-from dev --build dev
+BUILD_ERR=$(echo $?)
+
+if [ $BUILD_ERR -eq 0 ]; then
+    ssh root@$TARGET "/etc/init.d/packetd stop"; 
+    sleep 5
+    scp ./cmd/packetd/packetd root@$TARGET:/usr/bin/; 
+    ssh root@$TARGET "/etc/init.d/packetd start"
+fi
 
